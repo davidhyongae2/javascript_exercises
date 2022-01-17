6402d0774a98ba175dc8fb831ee280ff316a257a
David H. Gae <88872581+davidhyongae2@users.noreply.github.com>
2022-01-17 08:29:02 -0800

    Add files via upload

---
+++ java2.js
@@ -0,0 +1,17 @@
+function generateRandom(maxLimit = 100){
+  let rand = Math.random() * maxLimit;
+  //onsole.log(rand); //
+
+  rand = Math.floor(rand); // 99
+
+  return rand;
+}
+//generateRandom(10);
+
+for (let element=0; element<10000; element++) {
+    if ( generateRandom(10) == Number.Max_Value )
+		console.log(generateRandom(10)); //
+
+    }
+
+
