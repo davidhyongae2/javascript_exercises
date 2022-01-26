52cbf2e5c1770ef5b309e93d04029282890e4670
David H. Gae <88872581+davidhyongae2@users.noreply.github.com>
2022-01-26 13:27:42 -0800

    Update java4_grade12.js

--- java4_grade12.js
+++ java4_grade12.js
@@ -9,9 +9,8 @@ function num(a) {
 }
 
 
-//chemistry formula of the day grade 8
+//chemistry formula of the day grade 12
 for (let element=0; element<=4; element++) {
-	// think why starts at 83
 	for ( let degree=1; degree<=10;degree++) { 
     	console.log("answer:", numerator(2)/num(2)+1);
 		}	
