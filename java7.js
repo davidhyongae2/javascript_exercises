f0767ee7e78a6e58afe085961a1c7306f70eba3b
David H. Gae <88872581+davidhyongae2@users.noreply.github.com>
2022-02-08 08:14:14 -0800

    Add files via upload

---
+++ java7.js
@@ -0,0 +1,18 @@
+function num(f){
+  let c = (f+3)*(f-3);
+  //rand = float(rand); // 99
+  return c;
+}
+
+function new2(c){
+ var d = Math.sqrt(c)
+ return d;
+}
+
+//chemistry formula of the day grade 8
+// why is it NaN
+// use a pencil and paper to figure out why?
+for (let element=0; element<=3; element++) {
+	console.log("num:",num(element));
+    	console.log("hypotenuse:",new2(num(element)));
+		}	
