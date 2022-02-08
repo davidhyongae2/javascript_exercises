79a8c0ef9868e6e8eb8952548b183d34bb85a9d1
David H. Gae <88872581+davidhyongae2@users.noreply.github.com>
2022-02-08 08:06:31 -0800

    Update java6.js

--- java6.js
+++ java6.js
@@ -11,6 +11,7 @@ function conversionT(c) {
 //formula of the day grade 8
  	// think why it stops at 3.
 	for ( let x=0; x<=3;x++) { 
+	//what is the answer???
 	console.log("quadratic answer:",);
 	console.log("radian:",conversionT(x));
 		}
