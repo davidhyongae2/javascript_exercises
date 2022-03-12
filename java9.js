a45fcb03f5254a0242f32518a989ddf877cb3687
David H. Gae <88872581+davidhyongae2@users.noreply.github.com>
2022-03-12 03:13:38 -0800

    Add files via upload

---
+++ java9.js
@@ -0,0 +1,12 @@
+function circle(maxLimit){
+  //buffer
+  var b = 5;
+  let circle = 2 *b* 3.14 * maxLimit;
+  //console.log(rand); //
+
+  rand = Math.floor(rand); // 99
+
+  return rand;
+}
+var r = 100
+console.log(circle(r)); //
