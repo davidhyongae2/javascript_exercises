1c55bd758723656431be8b9cd4535f02105c8268
David H. Gae <88872581+davidhyongae2@users.noreply.github.com>
2022-03-12 02:44:26 -0800

    Add files via upload

---
+++ java10.js
@@ -0,0 +1,12 @@
+function triangle(maxLimit){
+  var b = 5;
+  var L = 5;
+  let rand = b*L * maxLimit;
+  //console.log(rand); //
+
+  rand = Math.floor(rand); // 99
+
+  return rand;
+}
+var r = 100
+console.log(triangle(r)); //
