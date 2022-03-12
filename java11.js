bc94c2e3cc85a1ba4a2cf04d0dc79f83df75a974
David H. Gae <88872581+davidhyongae2@users.noreply.github.com>
2022-03-12 03:05:40 -0800

    Add files via upload

---
+++ java11.js
@@ -0,0 +1,26 @@
+function trapezoid(maxLimit){
+  var b = 5;
+  var L = 5;
+  let rand = 0.5* (b*L) * maxLimit;
+  //console.log(rand); //
+
+  rand = Math.floor(rand); // 99
+
+  return rand;
+}
+var r = 100
+console.log(trapezoid(r)); //
+
+
+function rectangle(maxLimit) {
+  var w = 5;
+  var l = 5;
+  let rand = (w*l) * maxLimit;
+
+  rand = Math.floor(rand);
+
+  return rand;
+}
+
+var r = 100
+console.log(rectangle(r)); //
