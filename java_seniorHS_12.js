3defa2ec6e7888fc2059a1f9c149224bab6c5ab8
David H. Gae <88872581+davidhyongae2@users.noreply.github.com>
2022-04-03 09:03:08 -0700

    Add files via upload

---
+++ java_seniorHS_12.js
@@ -0,0 +1,14 @@
+function gen(maxLimit = 100){
+  const rand = Math.random()*maxLimit; 
+  //var rand = Math.random()*maxLimit;
+  //let rand = Math.random()*maxLimit;
+  return rand;
+}
+const custom = (process.argv[2]);
+//cs note: is this necessary? and explain.
+var num = gen(1)
+//let num = gen(1)
+//const num = gen(1)
+//6-8 grade mathematics
+console.log("generated percentage:",num,"input percentage:",num*custom);
+
